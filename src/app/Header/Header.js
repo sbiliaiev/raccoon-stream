f79f24ae977c3caa813fda5cb3dc2e20453ce11e
Serhii <serhii.biliaiev@gmail.com>
2016-12-12 02:56:37 +0200

    hope this will work

--- src/app/Header/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { PageHeader } from 'react-bootstrap';
-import './Header.css';
-
-const Header = () => <PageHeader className="header">Raccoon Stream <small>with love</small></PageHeader>
-
-export default Header;
